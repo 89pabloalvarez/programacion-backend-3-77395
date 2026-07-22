3a0a8fc8cef715bcd4153eb850705f80ade0a466
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-07-21 21:24:00 -0300

    agrego la ruta de usuarios al index del proyecto.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import { Router } from 'express'
 import { CONSTANTS as CONST } from '../common/constants.js'
 import productsRouter from './products.js'
 import cartsRouter from './carts.js'
+import usersRouter from './users.js'
 
 const router = Router()
 
 router.use(CONST.DIR_URL_PRODUCTS, productsRouter)
 router.use(CONST.DIR_URL_CARTS, cartsRouter)
+router.use(CONST.DIR_URL_USERS, usersRouter)
 
 export default router
