7ea9af44bf5c9dc1742b1daa2485b5e43abcd080
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-08-03 14:03:17 -0300

    agrego el loger en el server.js

--- src/server.js
+++ src/server.js
@@ -7,15 +7,20 @@ import apiRouter from './routes/index.js'
 import viewsRouter from './views/views-router.js'
 import registerProductSockets from './sockets/registerProductSockets.js'
 import { errorHandler } from './middlewares/errorHandler.js'
+import logger from './config/logger.js'
 
 const startServer = async () => {
+  logger.info('Inicializando ShipNow API')
   await mongoConnection()
 
   app.use(CONST.DIR_URL_ROOT, apiRouter)
   app.use('/', viewsRouter)
-  app.use(errorHandler)
+  app.use((req, res, next) => {
+    res.status(404).json({ status: 'error', code: 'ROUTE_NOT_FOUND', message: 'Ruta no encontrada', details: null })
+  })
 
   const serverHttp = app.listen(CONST.PORT, () => {
+    logger.info(`Servidor ShipNow escuchando en el puerto ${CONST.PORT}`)
     startupServer(CONST.BASEURL)
   })
 
