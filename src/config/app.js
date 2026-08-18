319a5e874d3f576303a261393e90a6e5c84ce196
DESKTOP-MAIN\Pablo Alvarez <89.pablo.alvarez@gmail.com>
2026-08-18 18:44:37 -0300

    Subo la config de "server.js" a "app.js" para poder switchear bien el ambiente de ejecución, ya sea por .env.test o .env dejando "StartServer()" limpio.

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,13 @@
 import express from 'express'
 import handlebars from 'express-handlebars'
+import swaggerUi from 'swagger-ui-express'
 import { loggerRequest, loggerResponse } from '../middlewares/logger.js'
+import { errorHandler } from '../middlewares/errorHandler.js'
+import { ERROR_DICTIONARY } from '../common/errors.js'
+import { CONSTANTS as CONST } from '../common/constants.js'
+import apiRouter from '../routes/index.js'
+import viewsRouter from '../views/views-router.js'
+import { swaggerSpec } from './swagger.js'
 
 const app = express()
 app.use(express.json())
@@ -18,5 +25,17 @@ app.engine('handlebars', handlebars.engine({
 }))
 app.set('views', 'src/views')
 app.set('view engine', 'handlebars')
+
+app.use(CONST.DIR_URL_ROOT, apiRouter)
+app.use('/', viewsRouter)
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
+app.use((req, res, next) => {
+  const err = new Error(ERROR_DICTIONARY.ROUTE_NOT_FOUND.message)
+  err.statusCode = ERROR_DICTIONARY.ROUTE_NOT_FOUND.statusCode
+  err.code = ERROR_DICTIONARY.ROUTE_NOT_FOUND.code
+  next(err)
+})
+app.use(errorHandler)
 
 export default app
